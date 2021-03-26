56f0038674e4b80865935dcddcad3f727b9237dd
MatiasKala <matiraffi@gmail.com>
2021-03-25 21:46:19 -0300

    Cambio de Repo

--- IntroVue/js/main.js
+++ IntroVue/js/main.js
@@ -3,9 +3,12 @@
     data: {
         mensaje: 'Soy un mensaje',
         valor:123,
+        valor2:999,
+        valor3:1000,
         contador:123,
         contador2:456,
-        contador3:789
+        contador3:789,
+        mostrar:true
 
     },
     methods:{
@@ -14,6 +17,11 @@
         },
         getContador(){
             return this.contador3
+        },
+        actualizar(e){
+            let dato=e.target.value
+            console.log('Actualizar',dato)
+            this.valor2=dato;
         }
     }
 })
