81808c56a4d51a6b5b1108cd83150603909e9934
MatiasKala <matiraffi@gmail.com>
2021-05-26 03:03:19 -0300

    Actualizado el archivo AJAX

--- Ajax/js/main.js
+++ Ajax/js/main.js
@@ -3,58 +3,7 @@ console.log(document.querySelector('title').textContent)
 const url = 'https://jsonplaceholder.typicode.com/posts/'
 
 /* ------------------------------------------------------------------------- */
-/*            ANIDADO DE PETICIONES ASINCRÓNICAS CON CALLBACKS               */
-/* ------------------------------------------------------------------------- */
-const getPostCb = (id,cb) => {
-    //creo la instancia de comunicación asincrónica AJAX con XMLHttpRequest
-    let xhr = new XMLHttpRequest
-    //configuro dicha instancia
-    xhr.open('get',url+id)
-    //registro el evento de fin de la comunicación
-    xhr.addEventListener('load', () => {
-      //la comunicación finalizó en forma exitosa
-      if(xhr.status == 200) {
-        let respuesta = JSON.parse(xhr.response)
-        //console.log(respuesta)
-        if(cb) cb(respuesta)
-      }
-      else {
-        console.error(`Error en GET -> status: ${xhr.status}`)
-      }
-    })
-    //envío la petición al servidor
-    xhr.send()
-}
-
-
-function getPostsCb() {
-
-    /* CALLBACK HELL -> infierno de callbacks ó pirámide de la perdición */
-    getPostCb(1, respuesta => {
-        console.log(respuesta)
-        getPostCb(2, respuesta => {
-            console.log(respuesta)
-            getPostCb(3, respuesta => {
-                console.log(respuesta)
-                getPostCb(4, respuesta => {
-                    console.log(respuesta)
-                    getPostCb(5, respuesta => {
-                        console.log(respuesta)
-                    })
-                })
-            })
-        })
-    })
-}
-
-/*
-console.log('Pedido de recursos')
-getPostsCb()
-console.log('Otras tareas...')
-*/
-
-/* ------------------------------------------------------------------------- */
-/*            ANIDADO DE PETICIONES ASINCRÓNICAS CON PROMESAS                */
+/*                  PETICIONES ASINCRÓNICAS CON PROMESAS                     */
 /* ------------------------------------------------------------------------- */
 const getPostPromise = id => {
     return new Promise((resolve,reject) => {
@@ -91,43 +40,61 @@ const getPostPromise = id => {
     })
 }
 
-function getPostsPromise() {
-    getPostPromise(1)
-    .then(respuesta => {
-        console.log(respuesta)
-        return getPostPromise(2)
-    })
-    .then(respuesta => {
-        console.log(respuesta)
-        return getPostPromise(3)
-    })
-    .then(respuesta => {
-        console.log(respuesta)
-        return getPostPromise(4)
-    })
-    .then(respuesta => {
-        console.log(respuesta)
-        return getPostPromise(5)
-    })
+/* ------------------------------------------------------------------ */
+/*  Uso de XMLHttpRequest con promesas utilizando sintaxis then catch */
+/* ------------------------------------------------------------------ */
+function getPostsPromiseThenCatch() {
+    getPostPromise(2)
     .then(respuesta => {
         console.log(respuesta)
     })
     .catch(error => console.log(error))
 }
 
-/*
-console.log('Pedido de recursos')
-getPostsPromise()
-console.log('Otras tareas...')
-*/
+/* ------------------------------------------------------------------- */
+/*  Uso de XMLHttpRequest con promesas utilizando sintaxis async await */
+/* ------------------------------------------------------------------- */
+//async function getPostsPromiseAsyncAwait() {
+//const getPostsPromiseAsyncAwait = async function() {
+const getPostsPromiseAsyncAwait = async () => {
+    try {
+        let respuesta = await getPostPromise(3)
+        console.log(respuesta)
+    }
+    catch(error) {
+        console.log(error)
+    }
+}
+
+//console.log('Pedido de recursos')
+//getPostsPromiseThenCatch()
+//getPostsPromiseAsyncAwait()
+//console.log('Otras tareas...')
 
 /* ------------------------------------------------------------------------- */
 /*                 PETICIONES ASINCRÓNICAS AJAX CON FETCH                    */
 /* ------------------------------------------------------------------------- */
-const getPostFetch = id => {
+const getPostFetchThenCatch = id => {
     fetch(url+id)
     .then(datos => datos.json())
     .then(respuesta => console.log(respuesta))
+    .catch(error => console.error(error))
 }
 
-getPostFetch(5)
+const getPostFetchAsyncAwait = async id => {
+    console.log('getPostFetchAsyncAwait -> inicio')
+    try {
+        let datos = await fetch(url+id)
+        let respuesta = await datos.json()
+        console.log(respuesta)
+    }
+    catch(error) {
+        console.error(error)
+    }
+    console.log('getPostFetchAsyncAwait -> fin')
+}
+
+//getPostFetchThenCatch(4)
+console.log('Inicio de peticiones')
+getPostFetchAsyncAwait(5)
+console.log('Otras tareas...')
